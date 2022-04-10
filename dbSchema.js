8e95455e94bb190839ba57fdefe0832fb8634744
Jyothsna <94163611+JyothsnaVellampalli@users.noreply.github.com>
2022-04-10 23:14:06 +0530

    Update dbSchema.js

--- dbSchema.js
+++ dbSchema.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const dbName = 'social';
-const dburl=`mongodb+srv://Jyothsna:Jyothsna123@cluster0.toued.mongodb.net/test${dbName}`;
+const dburl= process.env.MONGO_URL;
 
 module.exports = {MongoClient,dburl,mongodb}
